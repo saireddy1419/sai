677fba0773963f823204327be7310540faa57ff4
sandeep <kotha.sandeep5@gmail.com>
2022-09-27 01:05:24 +0100

    test try

---
+++ main/variables.tf
@@ -0,0 +1,32 @@
+variable "rg_name" {
+   description = "name of the resource group"
+   type = string
+   default     = "sai"
+}
+  
+variable "location" {
+   description = "Location where the resources will be created"
+   type = string
+   default     = "uksouth"
+}
+
+variable "tags" {
+   description = "Tags for the resources"
+   type = map(string)
+   default    = {
+         "environment" = "test"
+         "source" = "terraform" 
+   }
+  }
+
+  variable "org_name" {
+    description = "organisation name"
+    type = string
+    default = "sai"
+  }
+
+  variable "Project_name" {
+    description = "organisation name"
+    type = string
+    default = "firstproject"    
+  }
